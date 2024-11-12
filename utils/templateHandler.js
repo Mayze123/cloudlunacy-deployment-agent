a0c7f7e1d467289e42f4ab13e843afcaaa24b6a2
Mahamadou <m.taibou.i@gmail.com>
2024-11-11 21:33:40 -0500

    Some improvements

--- utils/templateHandler.js
+++ utils/templateHandler.js
@@ -324,7 +324,7 @@ class TemplateHandler {
     
     # Set environment variables
     ENV NODE_ENV=${environment}
-    ENV PORT=${deploymentPort}  // Changed this
+    ENV PORT=${deploymentPort}  
     
     # Create healthcheck endpoint
     RUN echo "const http=require('http');const server=http.createServer((req,res)=>{if(req.url==='/health'){res.writeHead(200);res.end('OK');}});server.listen(${deploymentPort});" > healthcheck.js  // Changed this
