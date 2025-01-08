787a42b4fbf5f7ff7d132b659ee33621c0816d9b
Mahamadou <m.taibou.i@gmail.com>
2025-01-08 13:08:42 -0500

    improv - v4.4.5

--- scripts/install-agent.sh
+++ scripts/install-agent.sh
@@ -412,9 +412,6 @@ services:
       - "27017:27017"
     networks:
       - internal
-    extra_hosts:
-      - "mongodb.cloudlunacy.uk:127.0.0.1"
-      - "mongodb:127.0.0.1"
     dns:
       - 8.8.8.8
       - 8.8.4.4
