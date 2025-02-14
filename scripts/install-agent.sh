7500a6620e40883e0c5474150f827154a2b0f9b4
Mahamadou <m.taibou.i@gmail.com>
2025-02-14 11:45:14 -0500

    improv - v8.0.3.0

--- scripts/install-agent.sh
+++ scripts/install-agent.sh
@@ -217,7 +217,7 @@ install_mongo() {
     log "Creating and starting MongoDB container..."
     docker run -d \
       --name mongodb-agent \
-      -p ${MONGO_PORT}:27017 \
+      -p 27017:27017 \
       -e MONGO_INITDB_ROOT_USERNAME=admin \
       -e MONGO_INITDB_ROOT_PASSWORD=adminpassword \
       mongo:latest
